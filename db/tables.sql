d1022d7ad32a2b5e5120e4ee45466487aae9344a
Jaime Lopez <detec.azul@gmail.com>
2018-08-14 13:18:48 +0100

    Added new tables on tables.sql

--- db/tables.sql
+++ db/tables.sql
@@ -1,5 +1,8 @@
-DROP TABLE IF EXISTS recipes;
+DROP TABLE IF EXISTS recipe_steps;
+DROP TABLE IF EXISTS quantities;
+DROP TABLE IF EXISTS measurements;
 DROP TABLE IF EXISTS ingredients;
+DROP TABLE IF EXISTS recipes;
 
 CREATE TABLE recipes(
   id SERIAL8 PRIMARY KEY,
@@ -10,6 +13,24 @@ CREATE TABLE recipes(
 CREATE TABLE ingredients(
   id SERIAL8 PRIMARY KEY,
   name VARCHAR(255),
-  description TEXT,
-  measure_type VARCHAR(255)
+);
+
+CREATE TABLE measurements(
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE quantities(
+  id SERIAL8 PRIMARY KEY,
+  recipe_id INT8 REFERENCES recipes(id) ON DELETE CASCADE,
+  ingredient_id INT8 REFERENCES ingredients(id) ON DELETE CASCADE,
+  measurement_id INT8 REFERENCES measurements(id) ON DELETE CASCADE,
+  ingredient_quantity FLOAT(8,2)
+)
+
+CREATE TABLE recipe_steps(
+  id SERIAL8 PRIMARY KEY,
+  recipe_id INT8 REFERENCES recipes(id) ON DELETE CASCADE,
+  step_number INT8,
+  step_description TEXT
 );
