b95bab7616919c3e1266e88826a9c2e0e2858e42
Jaime Lopez <detec.azul@gmail.com>
2018-08-14 13:22:27 +0100

    Added test elements on seed file

--- db/seed.sql
+++ db/seed.sql
@@ -6,6 +6,29 @@ SELECT * FROM recipes;
 
 INSERT INTO
 ingredients (name, description, measure_type)
-VALUES ('Egg', 'Oval and white', 'unit');
+VALUES ('Egg');
 
 SELECT * FROM ingredients;
+
+
+INSERT INTO
+measurements(name)
+VALUES ('unit');
+
+INSERT INTO
+measurements(name)
+VALUES ('pint');
+
+SELECT * FROM measurements;
+
+INSERT INTO
+quantities(recipe_id, ingredient_id, measurement_id, ingredient_quantity)
+VALUES(1, 1, 1, 4);
+
+SELECT * FROM quantities;
+
+INSERT INTO
+recipe_steps(recipe_id, step_number, step_description)
+VALUES(1, 1, 'Throw the eggs');
+
+SELECT * FROM recipe_steps;
