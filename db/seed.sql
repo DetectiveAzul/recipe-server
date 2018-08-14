b00719857f0654b006022c4acca80786e3e67c20
Jaime Lopez <detec.azul@gmail.com>
2018-08-14 12:57:39 +0100

    Added sql table for ingredients and a test ingredient on the seed file

--- db/seed.sql
+++ db/seed.sql
@@ -2,5 +2,10 @@ INSERT INTO
 recipes (name, description)
 VALUES ('Lasagna', 'Italian Pasta');
 
-
 SELECT * FROM recipes;
+
+INSERT INTO
+ingredients (name, description, measure_type)
+VALUES ('Egg', 'Oval and white', 'unit');
+
+SELECT * FROM ingredients;
--- db/tables.sql
+++ db/tables.sql
@@ -1,7 +1,15 @@
 DROP TABLE IF EXISTS recipes;
+DROP TABLE IF EXISTS ingredients;
 
 CREATE TABLE recipes(
   id SERIAL8 PRIMARY KEY,
   name VARCHAR(255),
   description TEXT
+);
+
+CREATE TABLE ingredients(
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255),
+  description TEXT,
+  measure_type VARCHAR(255)
 );
