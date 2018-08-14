1897daf11157debbd6a470cc230a16c1057d2016
Jaime Lopez <detec.azul@gmail.com>
2018-08-14 12:23:13 +0100

    Added CROSS origin request access

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
-import express from 'express';
-
+const express = require('express');
+const path = require('path');
 const app = express();
+const http = require('http').Server(app);
 const port = 3001;
 
-app.use(express.static('public'));
+const publicPath = path.join(__dirname, '../client/public');
+app.use(express.static(publicPath));
+
+//Allow Cross Origin Requests
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/', function (req, res) {
 
 });
 
-const server = app.listen(port, function() {
-  console.log('Listening on port: ', port);
+const server = http.listen(port, () => {
+  console.log('Example app at', port);
 });
