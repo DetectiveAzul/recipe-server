8b8d070a0392aa30d30ef60f24550b4269a18863
Jaime Lopez <detec.azul@gmail.com>
2018-08-17 12:18:03 +0100

    added CORS to server

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
 const indexRouter = require('./routers/indexRouter.js');
+const cors = require('@koa/cors');
 
 const app = new Koa();
 
 const PORT = process.env.PORT || 3001;
 
+app.use(cors());
 app.use(bodyParser());
 app.use(indexRouter.routes());
 
