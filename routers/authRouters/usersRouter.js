c87d5916a84225d40e04eca67f783f316c70576a
Jaime Lopez <detec.azul@gmail.com>
2018-08-21 14:46:22 +0100

    Added test view for registering users

--- routers/authRouters/usersRouter.js
+++ routers/authRouters/usersRouter.js
@@ -8,80 +8,28 @@ const BASE_URL = `/auth`;
 
 //REGISTER VIEW
 router.get(`${BASE_URL}/register`, async (ctx) => {
-  try {
-
-  } catch (err) {
-    ctx.status = 400;
-    ctx.body = {
-      status: 'error',
-      message: err.message || 'Sorry, an error has occurred.'
-    };
-  }
+  ctx.type = 'html';
+  ctx.body = fs.createReadStream('./views/register.html');
 });
 
 //REGISTER USER
 router.post(`${BASE_URL}/register`, async (ctx) => {
-  try {
-
-  } catch (err) {
-    ctx.status = 400;
-    ctx.body = {
-      status: 'error',
-      message: err.message || 'Sorry, an error has occurred.'
-    };
-  }
 });
 
 //LOGIN VIEW
 router.get(`${BASE_URL}/login`, async (ctx) => {
-  try {
-
-  } catch (err) {
-    ctx.status = 400;
-    ctx.body = {
-      status: 'error',
-      message: err.message || 'Sorry, an error has occurred.'
-    };
-  }
 });
 
 //LOG USER IN
 router.post(`${BASE_URL}/login`, async (ctx) => {
-  try {
-
-  } catch (err) {
-    ctx.status = 400;
-    ctx.body = {
-      status: 'error',
-      message: err.message || 'Sorry, an error has occurred.'
-    };
-  }
 });
 
 //STATUS PAGE
 router.get(`${BASE_URL}/status`, async (ctx) => {
-  try {
-
-  } catch (err) {
-    ctx.status = 400;
-    ctx.body = {
-      status: 'error',
-      message: err.message || 'Sorry, an error has occurred.'
-    };
-  }
 });
 
 //LOG USER OUT
 router.get(`${BASE_URL}/logout`, async (ctx) => {
-  try {
-
-  } catch (err) {
-    ctx.status = 400;
-    ctx.body = {
-      status: 'error',
-      message: err.message || 'Sorry, an error has occurred.'
-    };
-  }
 });
 
 
