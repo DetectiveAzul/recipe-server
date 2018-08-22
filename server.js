e7e41c965291d2bcbfca08acafbb12223cf4bb9b
Jaime Lopez <detec.azul@gmail.com>
2018-08-22 15:41:34 +0100

    Added authCheck method

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const Koa = require('koa');
 //Midleware
 const bodyParser = require('koa-bodyparser');
 const cors = require('@koa/cors');
-
+//Auth0 middleware
+const jwt = require('koa-jwt');
+const jwks = require('jwks-rsa');
 //Routers
 const indexRouter = require('./routers/indexRouter.js');
 
@@ -13,6 +15,23 @@ const PORT = process.env.PORT || 3001 ;
 // Cors and Bodyparser (for API management)
 app.use(cors());
 app.use(bodyParser());
+
+// Auth0
+
+const authCheck = jwt({
+  secret: jwks.koaJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 2,
+    // YOUR-AUTH0-DOMAIN name
+    jwksUri: "https://{YOUR-AUTH0-DOMAIN}/.well-known/jwks.json"
+  }),
+  // Identifier we set when we created the API
+  audience: '{YOUR-API-AUDIENCE-ATTRIBUTE}',
+  issuer: '{YOUR-AUTH0-DOMAIN}',
+  algorithms: [ 'RS256' ]
+});
+
 
 //Routers
 app.use(indexRouter.routes());
