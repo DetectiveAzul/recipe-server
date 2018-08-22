e770c05739a1b9cda7e655a4b40db825d4350397
Jaime Lopez <detec.azul@gmail.com>
2018-08-22 11:09:24 +0100

    User can login-logout. Cookie changes.

--- routers/authRouters/usersRouter.js
+++ routers/authRouters/usersRouter.js
@@ -32,24 +32,46 @@ router.post(`${BASE_URL}/register`, async (ctx) => {
 
 //LOGIN VIEW
 router.get(`${BASE_URL}/login`, async (ctx) => {
+  if (!ctx.isAuthenticated()) {
+    ctx.type = 'html';
+    ctx.body = fs.createReadStream('./views/login.html');
+    } else {
+    ctx.redirect('/auth/status');
+  };
 });
 
 //LOG USER IN
 router.post(`${BASE_URL}/login`, async (ctx) => {
+  return passport.authenticate('local', (err, user, info, status) => {
+    if (user) {
+      ctx.login(user);
+      ctx.redirect('/auth/status');
+    } else {
+      ctx.status = 400;
+      ctx.body = { status: 'error' };
+    }
+  })(ctx);
 });
 
 //STATUS PAGE
 router.get(`${BASE_URL}/status`, async (ctx) => {
-  if (ctx.isAuthenticated()) {
-  ctx.type = 'html';
-  ctx.body = fs.createReadStream('./views/status.html');
-} else {
-  ctx.redirect('/auth/login');
-}
+    if (ctx.isAuthenticated()) {
+      ctx.type = 'html';
+      ctx.body = fs.createReadStream('./views/status.html');
+    } else {
+      ctx.redirect('/auth/login');
+    }
 });
 
 //LOG USER OUT
 router.get(`${BASE_URL}/logout`, async (ctx) => {
+  if (ctx.isAuthenticated()) {
+    ctx.logout();
+    ctx.redirect('/auth/login');
+  } else {
+    ctx.body = { success: false };
+    ctx.throw(401);
+  }
 });
 
 
