8619a021221ab8e7fd4365eb33f0c22f97074bca
Jaime Lopez <detec.azul@gmail.com>
2018-08-29 10:31:23 +0100

    added advanced query to add a full recipe from front-end

--- db/queries/advancedQueries.js
+++ db/queries/advancedQueries.js
@@ -3,3 +3,43 @@ const ingredientsQueries = require('./ingredientsQueries.js');
 const measurementsQueries = require('./measurementsqueries.js');
 const quantitiesQueries = require('./quantitiesqueries.js');
 const stepsQueries = require('./stepsQueries.js');
+
+//ADVANCED QUERY
+const addFullRecipe = (recipe) => {
+  const recipeId = recipesQueries.addOne(recipe.info);
+
+  const ingedientIDs = []
+  const measurementIDs = []
+  const quantities = []
+
+  recipe.ingredients.forEach((ingredient) => {
+    ingredientIDs.push(ingredientsQueries.addOne({name: ingredient.ingredient}));
+    measurementIDs.push(measurementQueries.addOne({name: ingredient.measurement}));
+    quantities.push(ingredient.quantity);
+  });
+
+  quantities.forEach((quantity, index) => {
+    const newQuantity = {
+      recipeId: recipeId,
+      ingredientId: ingredientsIDs[index],
+      measurementId: measurementIDs[index],
+      ingredientQuantity: quantity
+    };
+    quantitiesQueries.addOne(newQuantity);
+  });
+
+  recipe.steps.forEach((step) => {
+    const newStep = {
+      recipeId: recipeId,
+      stepNumber: step.stepNumber,
+      stepDescription: step.stepDescription
+    };
+    stepsQueries.addOne(newStep);
+  });
+
+  return recipeId;
+};
+
+module.exports = {
+  addFullRecipe: addFullRecipe
+};
