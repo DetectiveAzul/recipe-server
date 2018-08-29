94540eb24fffbc0d740ad4a2a80212a1d49585c4
Jaime Lopez <detec.azul@gmail.com>
2018-08-29 10:17:09 +0100

    Adding advance query to save a recipe object received from db

---
+++ db/queries/advancedQueries.js
@@ -0,0 +1,5 @@
+const recipesQueries = require('./recipesQueries.js');
+const ingredientsQueries = require('./ingredientsQueries.js');
+const measurementsQueries = require('./measurementsqueries.js');
+const quantitiesQueries = require('./quantitiesqueries.js');
+const stepsQueries = require('./stepsQueries.js');
