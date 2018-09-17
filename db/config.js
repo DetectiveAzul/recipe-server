258d57e77330bad4a08d0c2abbaa68f3368db6dd
Jaime Lopez <detec.azul@gmail.com>
2018-09-17 12:53:46 +0100

    Heroku PSQL server

--- db/config.js
+++ db/config.js
@@ -1,8 +1,10 @@
 config = {
   databaseConfig: {
-    "host": "localhost",
+    "host": "ec2-54-247-101-191.eu-west-1.compute.amazonaws.com",
     "port": 5432,
-    "database": "recipe"
+    "database": "d3f42stsnodp2j",
+    "user": "uqlwpulfhlipjf",
+    "password": "5fd47aba9a34557028b4054c8649295d439aed5c4f257fc8368fb776a9a82de2"
   }
 };
 
